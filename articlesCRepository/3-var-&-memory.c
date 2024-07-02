9969ac1ca262d4f42f347f972b09c0ac459492d3
NayanaLoivos <loivosdev@gmail.com>
2024-07-02 16:39:34 -0300

    article 3: memory address and value

---
+++ articlesCRepository/3-var-&-memory.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int number1 = 10;
+    int number2;
+
+    printf("Posicao(&) na memoria de number1 %p:\n", &number1);
+    printf("Valor na memoria de number1 %d:\n", number1);
+    printf("Posicao(&) na memoria de number2 %p:\n", &number2);
+    printf("Valor na memoria de number2 %d:", number2);
+}
